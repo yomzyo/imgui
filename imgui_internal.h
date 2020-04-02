35d722178d60c314c420953c645971daf9974086
omar <omarcornut@gmail.com>
2020-04-02 16:46:53 +0200

    RangeSelect/MultiSelect: Fix testing key mods from after the nav request (remove need to hold the mod longer)

--- imgui_internal.h
+++ imgui_internal.h
@@ -1250,6 +1250,7 @@ struct ImGuiContext
     bool                    MultiSelectEnabled;
     ImGuiMultiSelectFlags   MultiSelectFlags;
     ImGuiMultiSelectState   MultiSelectState;                   // We currently don't support recursing/stacking multi-select
+    ImGuiKeyModFlags        MultiSelectKeyMods;
 
     // Render
     ImDrawData              DrawData;                           // Main ImDrawData instance to pass render information to the user
@@ -1424,6 +1425,7 @@ struct ImGuiContext
 
         MultiSelectEnabled = false;
         MultiSelectFlags = ImGuiMultiSelectFlags_None;
+        MultiSelectKeyMods = ImGuiKeyModFlags_None;
 
         DimBgRatio = 0.0f;
         BackgroundDrawList._OwnerName = "##Background"; // Give it a name for debugging
